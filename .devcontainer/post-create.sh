8f53b8014b9ec5fa2c3b4ac570884cbeed80e505
Zhang-Yu001 <1923299134@qq.com>
2026-08-04 15:49:33 +0800

    dev

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
-# This script is used to initialize devcontainer environment for msprof-analyze.
-# Copyright Huawei Technologies Co., Ltd. 2025-2026. All rights reserved.
+# -*- coding: utf-8 -*-
 # -------------------------------------------------------------------------
-# 容器首次创建后的幂等初始化脚本 (msprof-analyze)
-# 所有动作必须幂等，失败不阻塞容器创建
+# This file is part of the MindStudio project.
+# Copyright (c) 2026 Huawei Technologies Co.,Ltd.
+#
+# MindStudio is licensed under Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#
+#           http://license.coscl.org.cn/MulanPSL2
+#
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more details.
 # -------------------------------------------------------------------------
 
 log() { echo "[post-create] $*"; }
